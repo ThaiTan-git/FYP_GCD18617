c5675eb234101e47bab70e97dd46f17094e2c8ef
ThaiTan-git <63284265+ThaiTan-git@users.noreply.github.com>
2022-12-08 23:39:07 +0700

    MVC 3.1 Ver

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FYP_GCD18617.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
